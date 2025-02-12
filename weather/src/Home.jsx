5c73229fade896c624b6b8f68289777349a55f1e
ANKURJOSHI1991 <ankurjoshi1991@gmail.com>
2025-02-12 18:19:00 +0530

    weather project

---
+++ weather/src/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div className="container-fluid" style={{background:"linear-gradient(to bottom, Transparente 0%,Transparente 50%,red 50%,red 100%)"}}>
+            <div className="row">
+                <div className="col-md-4 offset-md-4">
+                    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat veritatis, accusantium natus incidunt nobis dolorem repudiandae, quas omnis cum reiciendis odit eaque consectetur. Ab possimus rerum enim vero amet odit.</h1>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
