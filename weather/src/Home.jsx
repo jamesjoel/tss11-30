b4c2b303765f310cf68cde8f3375f2f24204dbdc
Devesh14G <devesh140301gautam>
2025-02-13 10:49:35 +0530

    gradient work

--- weather/src/Home.jsx
+++ weather/src/Home.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState  , useEffect} from 'react'
+import axios from 'axios'
 
 const Home = () => {
+  let [allcity , setAllcity] = useState([])
+  let [allstate , setAllstate] = useState([])
+
+
+  useEffect(()=>{
+    axios
+   .get("")
+   .then(response=>{
+     // console.log(response.data);
+     setAllstate(response.data);
+   })
+}, [])
+
   return (
     <>
-        <div className="container-fluid" style={{background:"linear-gradient(to bottom, Transparente 0%,Transparente 50%,red 50%,red 100%)"}}>
-            <div className="row">
-                <div className="col-md-4 offset-md-4">
-                    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat veritatis, accusantium natus incidunt nobis dolorem repudiandae, quas omnis cum reiciendis odit eaque consectetur. Ab possimus rerum enim vero amet odit.</h1>
+        <div className='gradient_background container-fluid'>
+            <div className='row'>
+              <div className='col-md-4 offset-md-4 my-5'>
+                <div className='card my-5'>
+                  <div className='card-header'></div>
+                  <div className='card-body'>
+                    <label>State</label>
+                    <select className='form-control'>
+                      <option></option>
+                      {
+                         allstate.map(item=><option>{item}</option>)
+                      }
+                    </select>
+                    <label>city</label>
+                    <select className='form-control'>
+                      <option></option>
+                      { 
+                                    
+                         allcity.map((item, index)=>(<option>{item.name}</option>))
+                               
+                      }
+                    </select>
+                  </div>
+                  <div className='card-footer'></div>
                 </div>
+              </div>
             </div>
         </div>
     </>
