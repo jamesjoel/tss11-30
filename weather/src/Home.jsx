579dc2ad5994ad992a0580e47de417a8abcf5c2e
ANKURJOSHI1991 <ankurjoshi1991@gmail.com>
2025-02-17 10:54:42 +0530

    some changes

--- weather/src/Home.jsx
+++ weather/src/Home.jsx
@@ -1,52 +1,86 @@
-import React, { useState  , useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
-  let [allcity , setAllcity] = useState([])
-  let [allstate , setAllstate] = useState([])
+  
+  let [allCity,setAllCity] = useState([]);
+  let [allState,setAllState] = useState([]);
+  let [weather, setWeather] = useState();
+    useEffect(()=>{
+        axios
+        .get("https://tss11-30-project.onrender.com/api/v1/city/state")
+        .then(response=>{
+            //console.log(response.data)
+            setAllState(response.data)
+        })
+    },[])
 
+    let getState = ((e)=>{
+        let selectedState = e.target.value
+        axios
+        .get(`https://tss11-30-project.onrender.com/api/v1/city/getcity/${selectedState}`)
+        .then(response=>{
+            //console.log(response.data)
+            setAllCity(response.data)
+        })
+    })
 
-  useEffect(()=>{
-    axios
-   .get("")
-   .then(response=>{
-     // console.log(response.data);
-     setAllstate(response.data);
-   })
-}, [])
+    let getWeather = ((e)=>{
+      let selectedCity = e.target.value
+      axios
+      .get(`http://api.weatherapi.com/v1/current.json?key=017ae87f6b0e416391e100006251602&q=${selectedCity}&aqi=no`)
+      .then(response=>{
+        console.log(response.data)
+        setWeather(response.data)
+      
+    })
 
-  return (
+    })
+  
+    return (
     <>
-        <div className='gradient_background container-fluid'>
-            <div className='row'>
-              <div className='col-md-4 offset-md-4 my-5'>
-                <div className='card my-5'>
-                  <div className='card-header'></div>
-                  <div className='card-body'>
-                    <label>State</label>
-                    <select className='form-control'>
-                      <option></option>
+      <div className='gradient_background container-fluid'>
+        <div className='row d-flex justify-content-center'>
+          <div className="col-md-4 rounded-2xl shadow-lg shadow-purple-500 opacity-85 mt-5" style={{minHeight: '500px'}}>
+            <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                      <label>State</label>
+                      <select name='state' onChange={(e)=>getState(e)} className='form-control'>
+                      <option>Select</option>
                       {
-                         allstate.map(item=><option>{item}</option>)
+                        allState.map((item,index)=><option key={index}>{item}</option>)
                       }
-                    </select>
-                    <label>city</label>
-                    <select className='form-control'>
-                      <option></option>
-                      { 
-                                    
-                         allcity.map((item, index)=>(<option>{item.name}</option>))
-                               
+                     </select>
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                      <label>City</label>
+                      <select name='city' className='form-control' onChange={(e)=>getWeather(e)}>
+                      <option>Select</option>
+                      {
+                        
+                        allCity.map((item,index)=><option key={index}>{item.name}</option>)
+                      
                       }
-                    </select>
-                  </div>
-                  <div className='card-footer'></div>
-                </div>
-              </div>
+                      </select>
+                  </a>
+                </li>
+              </ul>
             </div>
+            <div className='text-dark'>
+              
+              {/* <h5 className='text-dark'>{weather.location.name}</h5>
+              <h5 className='text-dark'>{weather.current.temp_c}</h5> */}
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
