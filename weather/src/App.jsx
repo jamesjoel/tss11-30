8c53f44a42a570bdcf1c2a7b5b8418cf9385c537
jamesjoel <james.joelj@gmail.com>
2025-02-19 13:36:44 +0530

    home2

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Home from './Home'
+import Home2 from './Home2'
 
 const App = () => {
   return (
     <>
-    <Home />
+    <Home2 />
     </>
   )
 }
---
+++ weather/src/Home2.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import './Home2.css'
+const Home2 = () => {
+    let [bgImg, setBgImg] = useState("")
+  return (
+    <div className='main'>
+        <div className='weather-box' style={{backgroundImage :`url('${bgImg}')`}}>
+            <h3>Live Weacher App</h3>
+            <p>Search Your City</p>
+            <p>State</p>
+            <select className='form-control'></select>
+            <br />
+            <p>City</p>
+            <select className='form-control'></select>
+        </div>
+    </div>
+  )
+}
+
+export default Home2
